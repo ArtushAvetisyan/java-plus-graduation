f0a59e963268ca59cf7ffc598bdc424c6d3e0203
Artush Avetisyan <Artush.Avetis@yandex.com>
2026-07-15 15:01:49 +0300

    refactor(arch): migrate to microservices architecture with spring cloud
    - Move main-service under new core module
    - Add infra module with config-server, discovery-server, and gateway-server
    - Convert application properties to YAML format
    - Configure services to bootstrap using Spring Cloud Config

--- core/main-service/src/main/java/ru/practicum/Main.java
+++ core/main-service/src/main/java/ru/practicum/Main.java
@@ -6,8 +6,8 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class StatsServerApplication {
+public class Main {
     public static void main(String[] args) {
-        SpringApplication.run(StatsServerApplication.class, args);
+        SpringApplication.run(Main.class, args);
     }
 }
--- infra/gateway-server/src/main/java/ru/yandex/practicum/infra/gateway/server/GatewayServer.java
+++ infra/gateway-server/src/main/java/ru/yandex/practicum/infra/gateway/server/GatewayServer.java
@@ -1,4 +1,4 @@
-package ru.practicum;
+package ru.yandex.practicum.infra.gateway.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,8 +6,8 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class StatsServerApplication {
+public class GatewayServer {
     public static void main(String[] args) {
-        SpringApplication.run(StatsServerApplication.class, args);
+        SpringApplication.run(GatewayServer.class, args);
     }
 }
